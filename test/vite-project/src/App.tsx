6784ebd585cc6adc9a20531429076b78ca0ba5c1
Loop Shen <2947232977@qq.com>
2024-01-21 20:35:37 +0800

    样式初始化完成

--- test/vite-project/src/App.tsx
+++ test/vite-project/src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 
 // import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
 
     <div>
-      1
+      122
     </div>
 
   )
